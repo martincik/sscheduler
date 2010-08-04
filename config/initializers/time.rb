5995387a9ddd0202462e09d973f18022a87c9e56
Zdenal <zdenal@ubuntu.ubuntu-domain>
2010-08-04 19:03:45 +0200

    Table for products

--- config/initializers/time.rb
+++ config/initializers/time.rb
@@ -1,7 +1,7 @@
-Date::DATE_FORMATS[:eu] = "%d/%m/%Y"
+Date::DATE_FORMATS[:eu] = "%Y/%m/%d"
 Date::DATE_FORMATS[:eu_extend] = "%A, %d. %B, %Y"
 Time::DATE_FORMATS[:eu] = "%H:%M"
-Time::DATE_FORMATS[:eu_date] = "%d/%m/%Y"
-Time::DATE_FORMATS[:eu_datetime] = "%d.%m.%Y, %H:%M"
+Time::DATE_FORMATS[:eu_date] = "%Y/%m/%d"
+Time::DATE_FORMATS[:eu_datetime] = "%Y/%m/%d, %H:%M"
 Time::DATE_FORMATS[:tmp_format] = "%Y%m%d%H%M%S"
 
