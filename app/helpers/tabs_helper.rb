fcc5c97a34f8b48ddfaa6174d5c3801bce39ee85
ladislav.martincik <ladislav.martincik@xing.com>
2010-08-23 23:54:10 +0200

    Refactoring duplication!

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -1,10 +1,12 @@
 module TabsHelper
   # Create a tab as <li> and give it the id "current" if the current action matches that tab
   def tab(name, url, options = {})
+    html_options = {}
+    
     if controller.action_name =~ (options[:highlight] = /#{name}/i)
-      content_tag :li, link_to(options[:label] || name.to_s.capitalize, url, {:id => "current"})
-    else
-      content_tag :li, link_to(options[:label] || name.to_s.capitalize, url)
-    end    
+      html_options = {:id => "current"}
+    end
+    
+    content_tag :li, link_to(options[:label] || name.to_s.capitalize, url, html_options)    
   end
 end
