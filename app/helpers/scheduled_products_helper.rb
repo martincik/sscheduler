9a997990528a97829e2658e211a3639bbf6878d5
ladislav.martincik <ladislav.martincik@xing.com>
2010-08-26 17:43:54 +0200

    Cleanup of HTML and CSS.

--- app/helpers/scheduled_products_helper.rb
+++ app/helpers/scheduled_products_helper.rb
@@ -1,3 +1,11 @@
 module ScheduledProductsHelper
+  
+  def checkbox_with_selection_for_scheduled_product(product, is_checked, check_product)
+    check_box_tag("products[#{product.id}]", "1", is_checked, { 
+      :class => check_product.to_s,
+      :id => "product_#{product.id}"
+    })    
+  end
+  
 end
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,1 @@
-$(document).ready(function() {
-  $("button, input:submit").button();
-  $('#ajax-indicator').aqFloater({attach: 'n'});
-  $("#ajax-indicator").bind("ajaxSend", function(){
-     $(this).show();
-   }).bind("ajaxComplete", function(){
-     $(this).hide();
-   });
-
-});
 
