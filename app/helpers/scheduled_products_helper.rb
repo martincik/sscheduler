150f83c48c4ad3de01cd5634089ebd52742b72f1
Zdenal <zdenal@ubuntu.ubuntu-domain>
2010-09-15 17:02:36 +0200

    add thumb image of product to table

--- app/helpers/scheduled_products_helper.rb
+++ app/helpers/scheduled_products_helper.rb
@@ -1,11 +1,17 @@
 module ScheduledProductsHelper
-  
+
   def checkbox_with_selection_for_scheduled_product(product, is_checked, check_product)
-    check_box_tag("products[#{product.id}]", "1", is_checked, { 
+    check_box_tag("products[#{product.id}]", "1", is_checked, {
       :class => check_product.to_s,
       :id => "product_#{product.id}"
-    })    
+    })
   end
-  
+
+  def product_photo_thumb(product)
+    unless product.images.blank?
+      return image_tag product.images.first.src.gsub(/\.\w{3}\?/) {|s| "_thumb#{s}"}
+    end
+  end
+
 end
 
